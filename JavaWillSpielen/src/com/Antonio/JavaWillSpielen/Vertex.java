0d9f870b8530d76a79729cbf38dd8c7a1ff24afc
anton_4x37kdt <anton_4x37kdt@DESKTOP-DHKUO3F>
2021-09-24 09:16:51 +0200

    aufgabe 6

--- JavaWillSpielen/src/com/Antonio/JavaWillSpielen/Vertex.java
+++ JavaWillSpielen/src/com/Antonio/JavaWillSpielen/Vertex.java
@@ -5,7 +5,7 @@ public class Vertex {
 	public double x;
 	public double y;
 
-	public Vertex(double x, double y) {
+	public Vertex(double xx, double yy) {
 		this.x = x;
 		this.y = y;
 	}
@@ -13,6 +13,22 @@ public class Vertex {
 	public String toString() {
 		return "X: " + x + " | "+ "Y: " + y;
 
+	}
+	
+	public String addCoordsOne() {
+		return x + y + ": X and Y";
+	}
+	
+	public String addCoordsTwo() {
+		return "X and Y" + x + y ;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
 	}
 
 }
