389da28d92a1fc8860738b733afe2386d2ba6629
Escape9002 <antonio.rehwinkel@schiller-og.de>
2021-09-25 22:13:13 +0200

    Added "fUn"

--- StartLearning/src/com/Start/Antonio/Player.java
+++ StartLearning/src/com/Start/Antonio/Player.java
@@ -4,6 +4,7 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Player extends Canvas implements Runnable {
@@ -12,13 +13,19 @@ public class Player extends Canvas implements Runnable {
 	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
 	private Thread thread;
 	private boolean running = false;
-	
+
 	Scanner sc = new Scanner(System.in);
-	
+	Random r = new Random();
+
 	String firstName = "";
 	String sirName = "";
 	int age = 0;
 	int luckyNumber = 0;
+
+	int randNumber = 0;
+	private String[] BadWordStorage = { "Hello, fuck you", "There is a difference between letters and integers...",
+			"48 6f 70 65 49 74 48 75 72 74 73", "BrainCells.exe stoppped working", "42 79 65 48 61 76 65 53 68 69 74 54 69 6d 65",
+			"Appears you can read...USE IT", "Shit piss fuck cunt cocksucker motherfucker tit fart turd and twat", "https://www.youtube.com/watch?v=0SqhSfx2TkE", "..."};
 
 	static int ObjectCounter = 0;
 
@@ -42,10 +49,11 @@ public class Player extends Canvas implements Runnable {
 		}
 
 		return true;
-		
+
 	}
 
 	public void catchData() {
+		randNumber = r.nextInt(BadWordStorage.length);
 		String eingabe = "";
 
 		System.out.println("What's your First Name? (Please enter letters)");
@@ -72,17 +80,17 @@ public class Player extends Canvas implements Runnable {
 		eingabe = sc.next();
 
 		try {
-			
+
 			luckyNumber = Integer.parseInt(eingabe);
-			
-			if(luckyNumber < 0) {
+
+			if (luckyNumber < 0) {
 				System.out.println("Analphabetic");
 				new Window(WIDTH, HEIGHT, "TextGame", this);
-			}else if(luckyNumber >100) {
+			} else if (luckyNumber > 100) {
 				System.out.println("Analphabetic");
 				new Window(WIDTH, HEIGHT, "TextGame", this);
 			}
-			
+
 			System.out.println("Your Lucky Number: " + luckyNumber);
 
 		} catch (NumberFormatException e) {
@@ -94,7 +102,8 @@ public class Player extends Canvas implements Runnable {
 
 	public String outputData() {
 
-		return "-----------|| Player: " + ObjectCounter + " | " + "First Name: " + firstName + " | " + "Last Name: " + sirName + " | " + "Age: " +  age + " ||-----------";
+		return "-----------|| Player: " + ObjectCounter + " | " + "First Name: " + firstName + " | " + "Last Name: "
+				+ sirName + " | " + "Age: " + age + " ||-----------";
 
 	}
 
@@ -109,11 +118,11 @@ public class Player extends Canvas implements Runnable {
 	public String getSirName() {
 		return sirName;
 	}
-	
+
 	public int getLuckyNumber() {
 		return luckyNumber;
 	}
-	
+
 	public int GetObjectCounter() {
 		return ObjectCounter;
 	}
@@ -123,59 +132,57 @@ public class Player extends Canvas implements Runnable {
 		thread.start();
 		running = true;
 	}
-	
+
 	public synchronized void stop() {
 		try {
 			thread.join();
 			running = false;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	private void render() {
 		BufferStrategy bs = this.getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			this.createBufferStrategy(3);
 			return;
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
+
 		g.setColor(Color.black);
-		g.fillRect(0,0,WIDTH, HEIGHT);
-		
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+
 		g.setColor(Color.white);
-		g.drawString("Hello, fuck you", WIDTH/2, HEIGHT/2 );
-		
+		g.drawString(BadWordStorage[randNumber], WIDTH / 2-128, HEIGHT / 2);
+
 		g.dispose();
 		bs.show();
 	}
 
-	
 	public void run() {
 		this.requestFocus();
-	    long lastTime = System.nanoTime();
-	    double amountOfTicks = 60.0;
-	    double ns = 1000000000 / amountOfTicks;
-	    double delta = 0;
-	    long timer = System.currentTimeMillis();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
 //	    int frames = 0 ;
-	    while (running) {
-	        long now = System.nanoTime();
-	        delta += (now - lastTime) / ns;
-	        lastTime = now;
-	        if(running)
-	            render();
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if (running)
+				render();
 //	        frames++;
 
-	        if(System.currentTimeMillis() - timer > 1000) {
-	            timer += 1000;
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
 //	            System.out.println("FPS: " + frames);
 //	            frames = 0;
-	        }
-	    }
-	    stop();
+			}
+		}
+		stop();
 	}
 }
