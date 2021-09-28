2cfe06e0dcc5a4668f5d8b6443e9367d064ed27e
anton_4x37kdt <anton_4x37kdt@DESKTOP-DHKUO3F>
2021-09-28 15:25:23 +0200

    page 46

--- JavaWillSpielen/src/com/Antonio/JavaWillSpielen/TestVertex.java
+++ JavaWillSpielen/src/com/Antonio/JavaWillSpielen/TestVertex.java
@@ -4,17 +4,24 @@ public class TestVertex {
 
 	public static void main(String[] args) {
 		Vertex v1 = new Vertex(12, 30);
-		Vertex v2 = new Vertex(535, 2959);
-		Vertex v3 = new Vertex(0, 0);
+		Vertex v2 = new Vertex(1, 1);
+//		Vertex v3 = v1.skalarMult(42.0);
 
-		/*
-		System.out.println("v1.x: " + v1.x);
-		System.out.println("v1.y: " + v1.y);
-		*/
 		
 		System.out.println(v1);
-		System.out.println(v2);
-		System.out.println(v3);
+		
+		System.out.println(v1.skalarMult(2.0));
+		System.out.println(v1);
+		
+		v1.skalarMultMod(2.0);
+		System.out.println(v1);
+		
+		System.out.println(v1.add(v2));
+		System.out.println(v1);
+		
+		v1.addMod(v2);
+		System.out.println(v1);
+		
 	}
 
 }
--- JavaWillSpielen/src/com/Antonio/JavaWillSpielen/Vertex.java
+++ JavaWillSpielen/src/com/Antonio/JavaWillSpielen/Vertex.java
@@ -11,24 +11,47 @@ public class Vertex {
 	}
 
 	public String toString() {
-		return "X: " + x + " | "+ "Y: " + y;
+		return "X: " + x + " | " + "Y: " + y;
 
 	}
 	
-	public String addCoordsOne() {
-		return x + y + ": X and Y";
+	//--------------------------------------------page 46 OnGoing
+	public double length() {
+		return Math.sqrt(x * x + y * y);
+	}
+
+	public Vertex skalarMult(double s) {
+		return new Vertex(x * s, y * s);
+	}
+
+	public void skalarMultMod(double s) {
+		x = x * s;
+		y = y * s;
+	}
+
+	public Vertex add(Vertex v2) {
+		return new Vertex(x + v2.x, y + v2.y);
 	}
 	
-	public String addCoordsTwo() {
-		return "X and Y" + x + y ;
+	public void addMod(Vertex v3) {
+		x  =x+v3.x;
+		y  =y+v3.y;
 	}
-	
+
 	public double getX() {
 		return x;
 	}
 	
 	public double getY() {
 		return y;
+	}
+	
+	public double setX(double x) {
+		return this.x  =x;
+	}
+	
+	public double setY(double y) {
+		return this.y = y;
 	}
 
 }
