ca6b6f617426aa088545c17c0bc7e8f404622fff
anton_4x37kdt <anton_4x37kdt@DESKTOP-DHKUO3F>
2021-10-15 09:16:18 +0200

    Frame and Color

---
+++ JavaWillSpielen/src/com/Antonio/JavaWillSpielen/FirstPanel.java
@@ -0,0 +1,32 @@
+package com.Antonio.JavaWillSpielen;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class FirstPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void paintComponent(java.awt.Graphics g) {
+		g.setColor(Color.red);
+		g.drawRect(30, 50, 45, 80);
+		
+		g.setColor(Color.cyan);
+		g.fillRect(30, 50, 45, 80);
+		
+		g.setColor(Color.green);
+		g.drawString("Hello there", 90, 90);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
--- JavaWillSpielen/src/com/Antonio/JavaWillSpielen/TestVertex.java
+++ JavaWillSpielen/src/com/Antonio/JavaWillSpielen/TestVertex.java
@@ -8,11 +8,19 @@ public class TestVertex {
 		GeometricObject o3 = new GeometricObject(17);
 		GeometricObject o4 = new GeometricObject();
 
-		System.out.println(o1.equals(o2));
-		o2.moveTo(42, 23);
-		System.out.println(o1.equals(o2));
-		System.out.println(o3.contains(new Vertex(10, 3)));
-		System.out.println(o4);
+		SimpleOval ov1 = new SimpleOval(17,4,new Vertex(42,23));
+		SimpleRectangle rect1 = new SimpleRectangle (17,4,new Vertex(42,23));
+		SimpleRectangle rect2 = new SimpleRectangle (17,4,new Vertex(42,23));
+		
+//		System.out.println(o1.equals(o2));
+//		o2.moveTo(42, 23);
+//		System.out.println(o1.equals(o2));
+//		System.out.println(o3.contains(new Vertex(10, 3)));
+//		System.out.println(o4);
+		
+		System.out.println(ov1);
+		System.out.println(rect1.equals(rect2));
+		
 	}
 
 }
