67485bd91300272bf7fc5e3fbed645f18b5f1039
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2021-12-04 21:41:00 +0100

    Update Counter.java

--- JavaWillSpielen/src/com/Antonio/JavaWillSpielen/Counter.java
+++ JavaWillSpielen/src/com/Antonio/JavaWillSpielen/Counter.java
@@ -6,6 +6,31 @@ import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 
 import com.Antonio.util.ShowInFrame;
+/*
+https://docs.oracle.com/javase/tutorial/uiswing/events/intro.html
+
+public class MultiListener ... implements ActionListener {
+    ...
+    //where initialization occurs:
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+
+        button2.addActionListener(new Eavesdropper(bottomTextArea));
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        topTextArea.append(e.getActionCommand() + newline);
+    }
+}
+
+class Eavesdropper implements ActionListener {
+    ...
+    public void actionPerformed(ActionEvent e) {
+        myTextArea.append(e.getActionCommand() + newline);
+    }
+}
+
+*/
 
 public class Counter extends SimpleButton {
 
