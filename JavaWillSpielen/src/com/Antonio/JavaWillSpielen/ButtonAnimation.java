48e79a07635558545be552c6cba1d340b71d3e7c
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2021-12-07 14:22:46 +0100

    old pic not deleted after moving to new Vertex

---
+++ JavaWillSpielen/src/com/Antonio/JavaWillSpielen/ButtonAnimation.java
@@ -0,0 +1,41 @@
+package com.Antonio.JavaWillSpielen;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import com.Antonio.util.ShowInFrame;
+
+public class ButtonAnimation extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	JButton b = new JButton ("Push to move");
+	MovablePanel mp = new MovablePanel();
+	
+	ButtonAnimation (){
+		add(mp);
+		add(b);
+		
+		
+		b.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				mp.doOnTick();
+			}
+	});
+	}
+	
+	public static void main(String [] args) {
+		ButtonAnimation p = new ButtonAnimation();
+		p.mp.gos.add(new MovableImage("biene.png", 0,0,1,1));
+		p.mp.gos.add(new MovableImage("bat.png", 200,200,-1,-1));
+		ShowInFrame.show(p);
+	}
+	
+	
+}
