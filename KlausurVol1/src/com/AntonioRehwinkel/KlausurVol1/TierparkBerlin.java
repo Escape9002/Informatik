6a1594b7678d373a45a627f66b78e50bc865d0b4
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2023-02-08 21:25:34 +0100

    corrected utf8 errors

--- KlausurVol1/src/com/AntonioRehwinkel/KlausurVol1/TierparkBerlin.java
+++ KlausurVol1/src/com/AntonioRehwinkel/KlausurVol1/TierparkBerlin.java
@@ -6,16 +6,16 @@ public class TierparkBerlin {
 		
 		Hund hund = new Hund("Fredy", 18,5.0);
 		Schaeferhund SH = new Schaeferhund("ahd", 16, 5.0);
-		Chihauha Fuﬂhupe = new Chihauha("Fuck", 1 , 5.0);
+		Chihauha FuszHupe = new Chihauha("Fuck", 1 , 5.0);
 		
 		Besitzer Franz = new Besitzer(SH, ID.Besitzer, 25, "Franzie" );
 		
 		System.out.println(hund.getAllData());
 		System.out.println(SH.getAllData());
-		System.out.println(Fuﬂhupe.getAllData());
+		System.out.println(FuszHupe.getAllData());
 		
 		System.out.println(SH.getLautstaerkeDB());
-		System.out.println(Fuﬂhupe.getLautstaerkeDB());
+		System.out.println(FuszHupe.getLautstaerkeDB());
 		
 		System.out.println(Franz.getBack());
 
